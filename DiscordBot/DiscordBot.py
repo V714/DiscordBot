af18e4d07cc22819f213a8d2000dda48488377de
V714 <vlipczynski@gmail.com>
2018-05-28 22:39:21 +0200

    Monday night

--- DiscordBot/DiscordBot.py
+++ DiscordBot/DiscordBot.py
@@ -1,108 +1,100 @@
 import discord
 import asyncio
+from sqlf import load_db
 
-blockKane=False
+#...just don't ask
 blockNut=False
+nutCounter=0
+#counter of how much minutes bot is online
+counter=0
 
+#Friends IDs
 class idfnd:
     szefo="<@243142446274445322>"
     v7="<@280843168231063552>"
     nox="<@294905774767865857>"
     adwo="<@372478034147803158>"
+
+#My channels IDs
 class chnnl:
     owo="441604832588070933"
 
-def load_db():
-    db_file = open("database2.txt")
-    db=db_file.readlines()
-    nick,age,name=[''],[''],['']
-    for i in range(0,len(db)):
-        counter=0
-        data=''
-        for i2 in range(0,len(db[i])):
-            if db[i][i2] != "|":
-                data+=db[i][i2]
-            else:
-            
-                if counter==0:
-                    nick.append(data)
-                elif counter==1:
-                    age.append(data)
-                counter+=1
-                data=''
-            if i2 == len(db[i])-2:
-                name.append(data) 
-                data=''
-        
+def answer(message):
 
-    db_file.close()
-    database=''
-    for i in range(0,len(nick)):
-       database+=str('{:-<15}{:-^15}{:->15}'.format(nick[i],age[i],name[i]))
-       database+="\n"
-    return database 
+# There is the problem with 'content' attribute so I had to do exception
+    try:
+        a = message.content 
+        if a.startswith("hello V"):
+            return 'Witaj {0.author.mention} ^^'.format(message)
+        if a.startswith('szefo where are you'):
+            return '%s hunt' % (idfnd.szefo)
+        if a.startswith("lumos"):
+            return '%s' % (idfnd.nox)
 
+        if message.content.startswith('v!list'):
+            msg2=load_db()+"\n For {0.author.mention} :heart:!"
+            return msg2.format(message)
+
+    except AttributeError:
+        pass
+    
+    
 
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-    global blockKane
+
+    global nutMsg
+    global nutCounter
     global blockNut
-    clt = str(message.author)
-    # we do not want the bot to reply to itself
+
     if message.author == client.user:
         return
-    if message.content.startswith('hello V'):
-        msg = 'Witaj {0.author.mention} ^^'.format(message)
-        await client.send_message(message.channel, msg)
     
-    if message.content.startswith('bloknij kane'):
-        msg = 'Oczywiście {0.author.mention}!'.format(message)
-        blockKane=True
-        await client.send_message(message.channel, msg)
-    if message.content.startswith('uwolnić kane!'):
-        msg = 'No ok {0.author.mention}!'.format(message)
-        blockKane=False
-        await client.send_message(message.channel, msg)
-    if message.content.startswith('bloknij paffła'):
-        msg = 'Pawełek...przykro mi...'.format(message)
+    clt = str(message.author)
+
+    if message.content.startswith("bloknij paffła"):
         blockNut=True
-        await client.send_message(message.channel, msg)
-    if message.content.startswith('uwolnij paffła!'):
-        msg = 'Pawełek jesteś wolny!'.format(message)
+        nutMsg = await client.send_message(message.channel, 'Pawełek...przykro mi...')
+    if message.content.startswith("uwolnij paffła!"):
         blockNut=False
-        await client.send_message(message.channel, msg)
-    if message.content.startswith('szefo where are you'):
-        msg = '%s hunt'.format(message)
-        await client.send_message(message.channel, msg % idfnd.szefo)
-    if message.content.startswith('lumos'):
-        msg = '%s'.format(message)
-        await client.send_message(message.channel, msg % idfnd.nox)
-    if message.content.startswith('adwo chcesz bota?'):
-        msg = 'Nie oddawaj mje dla %s!'.format(message)
-        await client.send_message(message.channel, msg % idfnd.adwo)
-    if message.content.startswith('GetOutV'):
-        msg = ':sob:'.format(message)
-        await client.send_message(message.channel, msg)
-        quit()
+        nutCounter=0
+        client.send_message(message.channel,'Pawełek jesteś wolny!')
+    if blockNut:
+        #if clt == "Nutplace#0933":
+        if clt == "V7#8999":
+            await client.delete_message(message)
+            em = discord.Embed(title="Pawełek punched %d times!" % nutCounter, colour=0xFF0000)
+            await client.edit_message(nutMsg, embed=em)
+            nutCounter+=1
+    if message.content and answer(message):
+        await client.send_message(message.channel,answer(message))
     if message.content.startswith('V napisz'):
         msg = 'no siema mordo'.format(message)
         await client.send_message(message.author, msg)
-    if blockKane:
-        if clt == "kane#0214":
-            await client.delete_message(message)
-    if blockNut:
-        if clt == "Nutplace#0933":
-            await client.delete_message(message)
-    if message.content.startswith('v!list'):
-        msg2=load_db()+"\n For {0.author.mention} :heart:!"
-        msg = msg2.format(message)
-        await client.send_message(message.channel,msg)
+
+    if message.content.startswith('fajnie'):
+        await client.send_message(message.channel, 'Kto jest fajny? Napisz v! -imie-')
+
+        def check(msg):
+            return msg.content.startswith('v!')
+
+        message = await client.wait_for_message(author=message.author, check=check)
+        name = message.content[len('v!'):].strip()
+        await client.send_message(message.channel, '{} jest fajny!'.format(name))
+    answer(message.content)
+   
+
+    
+    
+
+
+    
 
 
 
-       
+# I guess this function runs just once
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -110,17 +102,22 @@ async def on_ready():
     print(client.user.id)
     print('------')
     await client.send_message(discord.Object(id=chnnl.owo),'żyje!')
-        
+    # Changing status    
     await client.change_status(discord.Game(name="VVVVVVVVVVVVVVVVVVVVVVVVVVVV"))
 
+# Some kind of while loop I guess
 async def my_background_task():
     await client.wait_until_ready()
-    counter = 0
+    global counter
+    # Spamming command to one of my channels...he just saying he is alive.
     while not client.is_closed:
         counter += 1
-        await client.send_message(discord.Object(id=chnnl.owo),'żyje! ' +str(counter))
+        await client.send_message(discord.Object(id=chnnl.owo),'żyje od {}min! :3'.format(str(counter)))
         await asyncio.sleep(60)
+
+# Taking Secret Token from Secret file :3
 filetoken=open("tkn.txt")
 token = filetoken.read()
 filetoken.close()
+# Login & Pass are in Token
 client.run(token)
---
+++ DiscordBot/sqlf.py
@@ -0,0 +1,35 @@
+﻿import pymysql
+import mysql.connector
+
+def load_db():
+    rf = open("rootpsw.txt")
+    rpsw = rf.readline()
+    rf.close()
+
+
+    conn = mysql.connector.connect(host='localhost',user='dicbot',passwd=rpsw,db='discordbot')
+    cursor = conn.cursor(buffered=True)
+
+    sql = ('SELECT nick FROM drrruserslist;')
+    cursor.execute(sql)
+    nick=cursor.fetchall()
+    sql = ('SELECT yo FROM drrruserslist;')
+    cursor.execute(sql)
+    yo=cursor.fetchall()
+    sql = ('SELECT name FROM drrruserslist;')
+    cursor.execute(sql)
+    name=cursor.fetchall()
+    database=''
+    n,y,m=[],[],[]
+    for el in nick:
+        n.append(str(el))
+    for el in yo:
+        y.append(str(el))
+    for el in name:
+        m.append(str(el))
+    conn.close()
+
+    for i in range(0,len(nick)):
+       database+=str('{:-<15}{:-^15}{:->15}'.format(n[i],y[i],m[i]))
+       database+="\n"
+    return database 
